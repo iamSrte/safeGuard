f224e65f33a3f91c745bdec384ac45ffa45c99b7
Sepehr Tavanaiee <iamsrte.pers@gmail.com>
2025-01-06 13:10:53 +0330

    Seperated web and db services into two different docker-compose files.

--- app/db.py
+++ app/db.py
@@ -1,11 +1,17 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+load_dotenv()
+PG_USERNAME = os.getenv('PG_USERNAME', 'NO_USERNAME')
+PG_PASSWORD = os.getenv('PG_PASSWORD', 'NO_PASSWORD')
+PG_DATABASE = os.getenv('PG_DATABASE', 'NO_DB_NAME')
+DATABASE_URL = os.environ.get('DATABASE_URL', f'postgresql://{PG_USERNAME}:{PG_PASSWORD}@localhost:5432/{PG_DATABASE}')
 
-URL_DATABASE = 'postgresql://iamsrte:12345678@db:5432/safeguard'
-
-engine = create_engine(URL_DATABASE)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
